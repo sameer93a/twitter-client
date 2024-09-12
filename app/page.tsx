cb5a7944441e4196676e20dc24d25a079a9d0df9
Sameer Ahmed <sameer937655@gmail.com>
2024-09-12 14:48:21 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,95 @@
+import { FeedCard } from "@/components/FeedCard";
+import Image from "next/image";
+import React from "react";
+import { BiHash, BiHomeCircle, BiUser } from "react-icons/bi";
+import { BsBell, BsBookmark, BsEnvelope, BsTwitter } from "react-icons/bs";
+import { MdAttachMoney } from "react-icons/md";
+import { SlOptions } from "react-icons/sl";
+
+interface TwitterSidebarButton {
+  title: string;
+  icon: React.ReactNode;
+}
+
+const sidebarMenuButton: TwitterSidebarButton[] = [
+  {
+    title: "Home",
+    icon: <BiHomeCircle />,
+  },
+  {
+    title: "Explore",
+    icon: <BiHash />,
+  },
+  {
+    title: "Notification",
+    icon: <BsBell />,
+  },
+  {
+    title: "Messages",
+    icon: <BsEnvelope />,
+  },
+  {
+    title: "Bookmarks",
+    icon: <BsBookmark />,
+  },
+  {
+    title: "Twitter Blue",
+    icon: <MdAttachMoney />,
+  },
+  {
+    title: "Profile",
+    icon: <BiUser />,
+  },
+  {
+    title: "Twitter Blue",
+    icon: <SlOptions />,
+  },
+];
+
+export default function Home() {
+  return (
+    <div>
+      <div className="grid grid-cols-12 h-screen px-80">
+        <div className="col-span-3">
+          <div className="text-2xl hover:bg-gray-800 rounded-full w-fit p-2 cursor-pointer transition-all">
+            <BsTwitter />
+          </div>
+          <div className="text-2xl font-medium pr-4">
+            <ul>
+              {sidebarMenuButton.map((item) => (
+                <li
+                  className="flex justify-start gap-4 items-center mt-4 hover:bg-gray-800 rounded-xl w-fit p-2 cursor-pointer"
+                  key={item.title}
+                >
+                  <span>{item.icon}</span>
+                  <span>{item.title}</span>
+                </li>
+              ))}
+            </ul>
+            <div className="mt-4">
+              <button className="bg-[#1d9bf0] py-3 rounded-full w-[90%] text-base font-semibold">
+                Tweet
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-6 border-l-[1px] border-r-[1px] border-gray-600 h-screen overflow-y-auto scrollbar-hide ">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+        </div>
+        <div className="col-span-3"></div>
+      </div>
+    </div>
+  );
+}
---
+++ components/FeedCard.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BiMessageRounded, BiUpload } from "react-icons/bi";
+import { FaRetweet } from "react-icons/fa";
+
+export const FeedCard = () => {
+  return (
+    <div className="border border-r-0 border-l-0 border-b-0 border-gray-600 p-5 hover:bg-slate-900 transition-all cursor-pointer">
+      <div className="grid grid-cols-12 gap-3">
+        <div className="col-span-1">
+          <Image
+            src={"https://avatars.githubusercontent.com/u/121109714?v=4"}
+            alt="user-image"
+            height={50}
+            width={50}
+            className="rounded-full"
+          />
+        </div>
+        <div className="col-span-11">
+          <h5>Sameer Ahmed</h5>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum,
+            consequuntur, nesciunt accusantium reprehenderit fugit tenetur
+            quisquam libero esse repellendus similique omnis voluptatem iusto
+            officiis unde soluta explicabo tempore odit. Hic?
+          </p>
+          <div className="flex justify-between text-xl items-center p-2 w-[90%]">
+            <div>
+              <BiMessageRounded />
+            </div>
+            <div>
+              <FaRetweet />
+            </div>
+            <div>
+              <AiOutlineHeart />
+            </div>
+            <div>
+              <BiUpload />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
